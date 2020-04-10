58603ccf0b7b0467465737d628e3bd69ea3de7ef
jAniceto <jose.pedro.aniceto@gmail.com>
2020-04-10 16:06:48 +0100

    Added source

--- src/App.js
+++ src/App.js
@@ -1,26 +1,99 @@
 import React from 'react';
-import logo from './logo.svg';
+import spells from './data/spells.json'
+import './css/skeleton.css'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.spells = spells.spells;
+    this.totalScore = this.spells.length;
+    this.state = {
+      spell: "",
+      guess: "",
+      score: 0,
+      count: 0
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+
+        <div className="header">
+          <img className="App-logo" alt="logo" src={process.env.PUBLIC_URL + '/hogwarts-logo.png'} /> 
+
+          <div>
+            <h1>Guess the Spell</h1>
+          </div>
+        </div>
+
+        <div className="content">
+
+          <div id="question">
+            <p><code>{this.state.spell.type}</code> {this.state.spell.effect}</p>
+          </div>
+
+          <div id="answer">
+            <input type="text" value={this.state.guess} onChange={this.changeInput} ref={(ip) => this.answerInput = ip} />
+          </div>
+
+          <button onClick={this.passQuestion}>Pass</button>
+
+          <div id="score">
+            <p>
+              Current score: {this.state.score} out of {this.state.count}
+              <br />
+              <small>Max score: {this.totalScore}</small>  
+            </p>
+          </div>
+
+        </div>
+        
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    this.getRandomQuestion();
+  }
+
+  getRandomQuestion = () => {
+    let spell = this.spells[Math.floor(Math.random() * this.spells.length)];
+    console.log(spell);
+    this.setState({
+      spell: spell,
+      guess: ""
+    });
+  }
+
+  changeInput = (event) => {
+    this.setState({
+      guess: event.target.value
+    }, this.checkGuess);
+  }
+
+  passQuestion = () => {
+    this.setState(prevState => {
+      return {
+        count: prevState.count + 1
+      }
+    }, this.getRandomQuestion, this.answerInput.focus());
+  }
+
+  checkGuess = () => {
+    if (this.state.guess.toLowerCase() === this.state.spell.incantation.toLowerCase()) {
+      // Correct answer
+      this.setState(prevState => {
+        return {
+          score: prevState.score + 1,
+          count: prevState.count + 1
+        }
+      }, this.getRandomQuestion);
+    } 
+  }
+
 }
 
 export default App;
